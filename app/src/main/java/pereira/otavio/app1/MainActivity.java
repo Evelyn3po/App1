6f007f8a18973742d98d7b3fa23b784fe079ffa6
20221TIIMI0351 <evelynpo59@gmail.com>
2023-03-15 12:46:54 -0300

    Código comentado por mim mesma, Evelyn linda!

--- app/src/main/java/pereira/otavio/app1/MainActivity.java
+++ app/src/main/java/pereira/otavio/app1/MainActivity.java
@@ -1,5 +1,5 @@
 package pereira.otavio.app1;
-
+//importando as classes
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,20 +8,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+//definindo a classe MainActivity que estende a classe AppCompatActivity
 public class MainActivity extends AppCompatActivity {
 
+    //o método OnCreate é chamada quando a Activity é iniciada
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Define o layout da atividade
         setContentView(R.layout.activity_main);
+        //realiza uma referência para o botão enviar
         Button btnEnviar= findViewById(R.id.btnEnviar);
+        // Define um listener para o botão enviar
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // realiza uma referência ao EditText para entrada de texto do layout
                 EditText etDigitAqui=findViewById(R.id.etDigiteAqui);
+                // realiza o texto inserido pelo usuário no EditText
                 String texto= etDigitAqui.getText().toString();
+                // Cria uma nova intenção para chamar a próxima atividade
                 Intent intent=new Intent(MainActivity.this,NextActivity.class);
+                // Adiciona o texto inserido pelo usuário como um extra para a intenção
                 intent.putExtra("texto",texto);
+                // Inicia a próxima atividade
                 startActivity(intent);
             }
         });
--- app/src/main/java/pereira/otavio/app1/NextActivity.java
+++ app/src/main/java/pereira/otavio/app1/NextActivity.java
@@ -1,20 +1,28 @@
 package pereira.otavio.app1;
 
+// Importando as classes necessárias
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+// Definindo a classe NextActivity que estende a classe AppCompatActivity
 public class NextActivity extends AppCompatActivity {
 
+    // O método onCreate é chamado quando a Activity é iniciada
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Define o layout da atividade
         setContentView(R.layout.activity_next);
+        // Pega a intenção que iniciou essa atividade
         Intent intent = getIntent();
+        // Pega o texto enviado pela atividade anterior como um extra
         String texto = intent.getStringExtra("texto");
+        // Pega uma referência ao TextView para exibir o texto recebido
         TextView tvExibir = findViewById(R.id.tvExibir);
+        // Define o texto recebido no TextView
         tvExibir.setText(texto);
     }
 }
